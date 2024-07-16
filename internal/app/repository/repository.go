7564f94d027f635d73eb249179b5dad8392cbc2e
Ilshat Minnibaev <ilshatminnibaev@gmail.com>
2024-07-16 22:07:36 +0200

    redunanrt methods in repo deleted, compose file fix

--- internal/app/repository/repository.go
+++ internal/app/repository/repository.go
@@ -2,8 +2,6 @@ package repository
 
 import (
 	"context"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.uber.org/zap"
@@ -66,20 +64,6 @@ func (r *Repository) StopRepository(ctx context.Context) {
 	r.Logger.Info("Repo started")
 }
 
-func (r *Repository) GetAll(ctx context.Context, db mongo.Database) ([]*model.Message, error) {
-	r.Collection = db.Collection("messages")
-
-	var entities []*model.Message
-	cursor, err := r.Collection.Find(ctx, bson.M{})
-	if err != nil {
-		return nil, err
-	}
-	if err := cursor.All(ctx, &entities); err != nil {
-		return nil, err
-	}
-	return entities, nil
-}
-
 func (r *Repository) Create(ctx context.Context, db mongo.Database, entity *model.Message) (*model.Message, error) {
 	r.Collection = db.Collection("messages")
 
@@ -88,62 +72,4 @@ func (r *Repository) Create(ctx context.Context, db mongo.Database, entity *mode
 		return nil, err
 	}
 	return entity, nil
-}
-
-func (r *Repository) GetById(ctx context.Context, db mongo.Database, id string) (*model.Message, error) {
-	r.Collection = db.Collection("messages")
-
-	objId, err := primitive.ObjectIDFromHex(id)
-	if err != nil {
-		return nil, err
-	}
-	var entity model.Message
-	err = r.Collection.FindOne(ctx, bson.M{"_id": objId}).Decode(&entity)
-	if err != nil {
-		return nil, err
-	}
-	return &entity, nil
-}
-
-func (r *Repository) Update(ctx context.Context, db mongo.Database, id string, entity *model.Message) (*model.Message, error) {
-	r.Collection = db.Collection("messages")
-
-	objId, err := primitive.ObjectIDFromHex(id)
-	if err != nil {
-		return nil, err
-	}
-
-	update := bson.M{"$set": entity}
-	opts := options.FindOneAndUpdate().SetReturnDocument(options.After)
-	result := r.Collection.FindOneAndUpdate(ctx, bson.M{"_id": objId}, update, opts)
-	if err = result.Err(); err != nil {
-		return nil, err
-	}
-
-	err = result.Decode(&entity)
-	if err != nil {
-		return nil, err
-	}
-	return entity, nil
-}
-
-func (r *Repository) Delete(ctx context.Context, db mongo.Database, id string) error {
-	r.Collection = db.Collection("messages")
-
-	objId, err := primitive.ObjectIDFromHex(id)
-	if err != nil {
-		return err
-	}
-	res, err := r.Collection.DeleteOne(ctx, bson.M{"_id": objId})
-	if err != nil {
-		return err
-	}
-	if res.DeletedCount == 0 {
-		return err
-	}
-	return nil
-}
-
-func (r *Repository) GetConnected() {
-
 }
